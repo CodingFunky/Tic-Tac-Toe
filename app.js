baa2a1f288e9649831ccb51365e26e53e8736636
BananaBJones <95904947+CodingFunky@users.noreply.github.com>
2022-01-02 08:42:34 -0700

    overlay, remove event listeners properly

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+const body = document.querySelector('body');
 const boardEle = document.querySelector('#board');
 const boardSpot = document.querySelectorAll('.boardSpot');
 const winnerEle = document.querySelector('#winner');
 const resetBtn = document.querySelector('#reset');
 const submitBtn = document.querySelector('#submit');
 const playerName = document.querySelector('#playerName');
+const overlay = document.querySelector('.overlay');
 
 const player = (sign, name) => {
     return {sign, name};
 };
 
 let playerOne = player('X', 'X');
-let playerTwo = player('O', 'X');
+let playerTwo = player('O', 'O');
 let whoseTurn = playerOne;
 
 submitBtn.addEventListener('click', () => {
     playerOne.name = playerName.value;
 })
 
-const displayController = (() => {
+const displayController = ((e) => {
     let update = function(spot) {
         if(!spot.textContent && whoseTurn === playerOne) {
             spot.textContent = "X";
@@ -49,26 +51,47 @@ const gameBoard = (() => {
     }
     let checkWinner = function(spotIndex) {
         const tieGame = (spot) => spot === '';
-        if (board[0].sign === board[1].sign && board[0].sign === board[2].sign && board[1] != '') {
+        console.log(board);
+        if (board[0] === board[1] && board[0] === board[2] && board[1] != '') {
+            gameBoard.endGame();
             winnerEle.textContent = board[0].name + ' Wins';
-        }else if (board[0].sign === board[4].sign && board[0].sign === board[8].sign && board[4] != '') {
+        }else if (board[0] === board[4] && board[0] === board[8] && board[4] != '') {
             winnerEle.textContent = board[0].name + ' Wins';
-        }else if (board[0].sign === board[3].sign && board[0].sign === board[6].sign && board[3] != '') {
+            gameBoard.endGame();
+        }else if (board[0] === board[3] && board[0] === board[6] && board[3] != '') {
             winnerEle.textContent = board[0].name + ' Wins';
-        }else if (board[1].sign === board[4].sign && board[1].sign === board[7].sign && board[4] != ''){
+            gameBoard.endGame();
+        }else if (board[1] === board[4] && board[1] === board[7] && board[4] != ''){
             winnerEle.textContent = board[1].name + ' Wins';
-        }else if (board[2].sign === board[4].sign && board[2].sign === board[6].sign && board[4] != '' || board[2].sign === board[5].sign && board[2].sign === board[8].sign && board[5] != '') {
+            gameBoard.endGame();
+        }else if (board[2] === board[4] && board[2] === board[6] && board[4] != '' || board[2] === board[5] && board[2] === board[8] && board[5] != '') {
             winnerEle.textContent = board[2].name + ' Wins';
-        }else if (board[3].sign === board[4].sign && board[3].sign === board[5].sign && board[4] != ''){
+            gameBoard.endGame();
+        }else if (board[3] === board[4] && board[3] === board[5] && board[4] != ''){
             winnerEle.textContent = board[3].name + ' Wins';
-        }else if (board[6].sign === board[7].sign && board[6].sign === board[8].sign && board[7] != '') {
+            gameBoard.endGame();
+        }else if (board[6] === board[7] && board[6] === board[8] && board[7] != '') {
             winnerEle.textContent = board[6].name + ' Wins';
+            gameBoard.endGame();
         }else if (board.findIndex(tieGame) < 0) {
             alert('Tie');
+            gameBoard.endGame();
         }
         else {
             return 0;
         }
+    }
+    let endGame = function() {
+        // overlay.classList.add('active');
+        let text = document.createElement('div');
+        text.classList.add('winnerText')
+        text.textContent = whoseTurn.name + ' Wins!';
+        overlay.appendChild(text);
+        overlay.onclick = function closeOverlay() {
+            overlay.classList.remove('active');
+            gameBoard.reset();
+            text.textContent = '';
+          }
     }
     let reset = function() {
         board = ['', '', '', 
@@ -76,18 +99,23 @@ const gameBoard = (() => {
                  '', '', ''];
         displayController.reset();
         whoseTurn = playerOne;
+        winnerEle.textContent = 'Results';
     }
-    return {board, update, checkWinner, reset};
+    return {board, update, checkWinner, reset, endGame};
     
 })();
 boardSpot.forEach((spot) => {
+    // if (winnerEle.textContent != 'Results') return;
     spot.addEventListener('click', () => {
+        if (winnerEle.textContent != 'Results') return;
         displayController.update(spot);
     })
 })
+// boardSpot.forEach((spot) => {
+//     spot.addEventListener('click', displayController.update(spot));
+// })
 resetBtn.addEventListener('click', () => {
     gameBoard.reset();
-    winnerEle.textContent = 'Results';
 })
 // const gameLoop = (() => {
 
